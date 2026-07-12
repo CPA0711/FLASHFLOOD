a5f10492479ec82ecd06d644052282fa5bb87c01
CPA0711 <0711.cpa@gmail.com>
2026-07-12 11:34:19 +0700

    Update flashV2.py

--- flashV2.py
+++ flashV2.py
@@ -267,7 +267,7 @@ def main(argv):
         sys.exit(2)
 
     for opt, arg in opts:
-    if opt in ('-h', '--help'):
+            if opt in ('-h', '--help'):
         showUsage()
         sys.exit(2)
     elif opt in ('-v', '--url'):
